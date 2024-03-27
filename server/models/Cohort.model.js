4c8e7e72d19bfb8864b4f24651723a07dab493d3
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-27 21:22:41 +0100

    updated cohort and student models

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -2,18 +2,63 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const StudentSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  phone: String,
-  linkedinUrl: String,
-  languages: [String],
-  program: String,
-  background: String,
-  image: String,
-  cohort: Number,
-  projects: [String],
+const cohortSchema = new Schema({
+  cohortSlug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  cohortName: {
+    type: String,
+    required: true,
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+    required: true,
+  },
+  format: {
+    type: String,
+    enum: ["Full Time", "Part Time"],
+  },
+  campus: {
+    type: String,
+    enum: [
+      "Madrid",
+      "Barcelona",
+      "Miami",
+      "Paris",
+      "Berlin",
+      "Amsterdam",
+      "Lisbon",
+      "Remote",
+    ],
+  },
+  startDate: {
+    type: Date,
+    default: Date.now,
+  },
+  endDate: {
+    type: Date,
+  },
+  inProgress: {
+    type: Boolean,
+    default: false,
+  },
+  programManager: {
+    type: String,
+    required: true,
+  },
+  leadTeacher: {
+    type: String,
+    required: true,
+  },
+  totalHours: {
+    type: Number,
+    default: 360,
+  },
 });
 
-module.exports = mongoose.model("Student", StudentSchema);
+const Cohort = mongoose.model("Cohort", cohortSchema);
+
+module.exports = Cohort;
