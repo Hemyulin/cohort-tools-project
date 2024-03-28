4210e22733bb2c6c83116d3052163160384c4976
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-28 14:21:49 +0100

    tiny changes in code order

--- server/app.js
+++ server/app.js
@@ -5,8 +5,11 @@ const PORT = 5005;
 const cohorts = require("./cohorts.json");
 const students = require("./students.json");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const app = express();
+
+const mongoose = require("mongoose");
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohort.model");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017")
@@ -39,13 +42,37 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+// Student Routes
+
+// POST /api/students - Creates a new student
+
+// GET /api/students - Retrieves all of the students in the database collection
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
+
+// GET /api/students/cohort/:cohortId - Retrieves all of the students for a given cohort
+
+// GET /api/students/:studentId - Retrieves a specific student by id
+
+// PUT /api/students/:studentId - Updates a specific student by id
+
+// DELETE /api/students/:studentId - Deletes a specific student by id
+
+// Cohort Routes
+
+// POST /api/cohorts - Creates a new cohort
+
+// GET /api/cohorts - Retrieves all of the cohorts in the database collection
 app.get("/api/cohorts", (req, res) => {
   res.json(cohorts);
 });
 
-app.get("/api/students", (req, res) => {
-  res.json(students);
-});
+// GET /api/cohorts/:cohortId - Retrieves a specific cohort by id
+
+// PUT /api/cohorts/:cohortId - Updates a specific cohort by id
+
+// DELETE /api/cohorts/:cohortId - Deletes a specific cohort by id
 
 // START SERVER
 app.listen(PORT, () => {
